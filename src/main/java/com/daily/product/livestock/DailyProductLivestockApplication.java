4756f84d3d285854503c69f7556e0fc40b1f82af
venh <conf312@naver.com>
2023-01-08 03:26:51 +0900

    initialize project

---
+++ src/main/java/com/daily/product/livestock/DailyProductLivestockApplication.java
@@ -0,0 +1,13 @@
+package com.daily.product.livestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyProductLivestockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyProductLivestockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daily/product/livestock/DailyProductLivestockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daily.product.livestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyProductLivestockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
